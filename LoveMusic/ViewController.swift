d0a397658f60eb541663f47a14e6423f3696c3aa
MD. Mueez <mueez@MDs-Mac.local>
2019-09-15 19:15:01 -0700

    Initial Commit

---
+++ LoveMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveMusic
+//
+//  Created by MD. Sheersho Mueez on 9/15/19.
+//  Copyright © 2019 Cookie Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
