5927c4069a48394f8c937f3f8213e74833cf83c8
mueez_mac <mueezsheersho95@gmail.com>
2019-09-18 18:28:56 -0700

    update1

--- LoveMusic/ViewController.swift
+++ LoveMusic/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import SQLite3
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    var db: OpaquePointer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let fileurl_time = try!
+            FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("time_sqp.sqlite")
+        
+        if sqlite3_open(fileurl_time.path, &db) != SQLITE_OK {
+            print("something went wrong")
+        }
+        
+        let createTableTime = "CREATE TABLE IF NOT EXISTS time(id INTEGER PRIMARY KEY, hour INTEGER, min INTEGER, sec INTEGER, bela TEXT, sat BOOLIAN, sun BOOLIAN, mon BOOLIAN, tues BOOLIAN,  )"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+   
+    
 }
 
